92d95b3b696e04d03296ddb0325f577024d67832
Catty <kata.chavez@gmail.com>
2021-09-27 11:30:54 -0300

    Python as a calculator

---
+++ hello-world.py
@@ -0,0 +1,18 @@
+#Esta línea es un comentario
+print(1000) #Esto tambien es un comentario de una línea
+
+"""Esto es 
+un comentario
+multilínea"""
+
+print(round(1000 * 1.19)) #función round quita los decimales
+
+#Cuando se calcula en python, hay que revisar la precedencia, se deben ocupar los parentesis.
+print(round(12 + 10 * 2)) #En este caso dará 32
+
+#Se deben ocupar los parentesis
+print(round((12 + 10) * 2)) #En este caso dará 44
+#orden de precedencia: raiz, multiplicación, división, suma, resta
+
+
+
